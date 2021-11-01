59ac546edc9be2722588bc99e85e36b3cff1a937
Ali Sever <alisever96@hotmail.com>
2021-11-01 18:53:30 +0000

    New functions.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="matiq",
-    version="0.0.11",
+    version="0.0.12",
     author="Ali Sever",
     author_email='alisever96@hotmail.com',
     description="Package for Matiq Tests",
